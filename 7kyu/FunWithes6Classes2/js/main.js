83a0d60f91bc0dc952c2a345e116223bc7a115b4
jordan Moldovan <jmoldyvan@gmail.com>
2022-03-28 14:05:03 -0700

    03/28/22

---
+++ 7kyu/FunWithes6Classes2/js/main.js
@@ -0,0 +1,23 @@
+// Get Coding :)
+
+class Shark extends Animal{
+    constructor(name,age,status){
+        super(name, age, 0, "shark", status);
+  }
+}
+class Cat extends Animal{ constructor(name,age,status){
+    super(name, age, 4, "cat", status);
+  }
+  introduce(){
+      return super.introduce() + ' Meow meow!';
+  }
+}
+class Dog extends Animal{ constructor(name,age,status){
+    this.name = name,
+    this.age = age,
+    this.status = status,
+
+super(legs)
+super(species)
+}
+}
