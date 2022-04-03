84a89dc2a20a7f32f7964933203a39fa93c2af27
jordan Moldovan <jmoldyvan@gmail.com>
2022-04-03 09:00:34 -0700

    04/03/22

---
+++ 8kyu/Welcome!/js/main.js
@@ -0,0 +1,32 @@
+let greetings = {
+    english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+}
+
+
+function greet(language) {
+	for (let x in greetings) {
+        if (x === language) {
+           return greetings[x]
+        }
+            
+    }
+    return 'Welcome'
+}
+
+console.log(greet('english'))
