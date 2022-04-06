62b708c48067f77c4d3be7b3d36e447f704a0834
jordan Moldovan <jmoldyvan@gmail.com>
2022-04-06 11:32:59 -0700

    5/10/22

---
+++ 6kyu/FindtheOddInt/js/main.js
@@ -0,0 +1,17 @@
+function findOdd(A) {
+    let x = [];
+    let y = [];
+    const counts = {};
+    A.forEach(function (e) {
+        counts[e] = (counts[e] || 0) + 1;
+    });
+    x.push(Object.keys(counts))
+    y.push(Object.values(counts))
+    x = x.join('').split(',')
+    y = y.join('').split(',')
+    const finodd = (element) => (element % 2 !== 0);
+    let isodd = y.findIndex(finodd)
+    return +(x[isodd]);
+}
+
+findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])
