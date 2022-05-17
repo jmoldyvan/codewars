c6b68d837c862ca5f06a30b97a2c703b9d6ff0b4
jordan Moldovan <jmoldyvan@gmail.com>
2022-05-16 18:30:17 -0700

    05/16/22

--- 7kyu/CodingMeetup6/js/main.js
+++ 7kyu/CodingMeetup6/js/main.js
@@ -1,5 +1,5 @@
 function isSameLanguage(list) {
     // thank you for checking out the Coding Meetup kata :)
-    return list.every(element => element.language === list[0].languge)
+    return list.every(element => element.language === list[0].language)
     
   }
