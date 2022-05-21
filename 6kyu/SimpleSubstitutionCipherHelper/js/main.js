8e9940fc48eed79055bf4199aa49f2fbb438c4e8
jordan Moldovan <jmoldyvan@gmail.com>
2022-05-21 13:54:58 -0700

    05/21/22

---
+++ 6kyu/SimpleSubstitutionCipherHelper/js/main.js
@@ -0,0 +1,8 @@
+function SubstitutionCipher(abc1, abc2) {
+    this.encode = function (s) {
+        return s.split('').map(function(_){ return t[f.indexOf(_)] || _; }).join('');
+      }
+      this.decode = function (s) {
+        return s.split('').map(function(_){ return f[t.indexOf(_)] || _; }).join('');
+      }
+    }
