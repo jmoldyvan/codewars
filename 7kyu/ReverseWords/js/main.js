4f98f7ce9b3519eab581c569fbe740cb30898934
jordan Moldovan <jmoldyvan@gmail.com>
2022-06-10 18:35:07 -0700

    06/10/22

---
+++ 7kyu/ReverseWords/js/main.js
@@ -0,0 +1,8 @@
+function reverseWords(str) {
+    str = str.split(' ')
+    console.log(str);
+    result = str.map((e) => Array.from(e).reverse().join(''))
+    return result.join('')
+  }
+
+  reverseWords('The quick brown fox jumps over the lazy dog.')
