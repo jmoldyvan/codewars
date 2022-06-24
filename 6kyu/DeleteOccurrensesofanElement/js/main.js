1350337ba7618796ca0bbed7f3dd1640c5dedadb
jordan Moldovan <jmoldyvan@gmail.com>
2022-06-24 11:34:16 -0700

    date

--- 6kyu/DeleteOccurrensesofanElement/js/main.js
+++ 6kyu/DeleteOccurrensesofanElement/js/main.js
@@ -1,5 +1,15 @@
 function deleteNth(arr,n){
     
-    
+    const occ = []
+
+    return arr.filter(x => {
+        if(occ[x]){
+            occ[x]++
+        }
+        else{
+            occ[x] = 1
+        }
+        return occ[x] <= n
+    })
 
   }
