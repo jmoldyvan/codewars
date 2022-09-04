f528c605f8941906d584154e36b1162ad357774a
jordan Moldovan <jmoldyvan@gmail.com>
2022-09-04 14:05:49 -0700

    sep

---
+++ grind75/easy/CountingBits/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+ var countBits = function (n) {
+    n++;                                             // now n is n+1 ##
+
+    let nums = [0];
+    while (nums.length < n) {
+        nums = [
+            ...nums,                                 // same
+            ...nums.map((num) => num + 1),           // same but next numbers
+        ];
+    }
+    nums.length = n;                                 // we only want 1st n+1 (see above ##) numbers, delete rest
+    return nums;
+};
