74dfbdcb26c08b659e2ead136fed2cdc8ec65a40
jordan Moldovan <jmoldyvan@gmail.com>
2022-09-13 16:22:26 -0700

    sep

--- 6kyu/practiceParse/js/main.js
+++ 6kyu/practiceParse/js/main.js
@@ -3,3 +3,49 @@
 
 // Whitespace characters can be anywhere (except between numeric digits)
 // The "*" could be any of "*", "/", "+", "-"
+
+function parsethis(input){
+
+    const numb = input.split(/\D/g);
+    const op = input.split(/\d/g).filter(Boolean);
+    numbers=[]
+    numb.forEach(element => {
+        numbers.push( Number(element))
+    });
+    let merged = []
+    for (let i = 0; i < numbers.length; i++) {
+       merged.push(numbers[i])
+       merged.push(op[i])
+    }
+    merged.pop()
+    console.log(merged);
+
+//     let stack = []
+//     let prevSign = '+'
+//     let num = ''
+
+// for( let i = 0; i<input.length; i++){
+//     // if input[i] is a number. not NaN after number type change
+//     if(isNaN(input[i])){
+//         // put input[i] into string
+//         num+=input[i]
+//     }
+//     if(isNaN(input[i]) || i == input.length - 1){
+        
+//         if(prevSign == '+'){
+//             stack.push(Number(num))
+//         }
+//         else if(prevSign == '-'){
+//             stack.push(Number(-num))
+//         }
+//         else if(prevSign == '/'){
+//             stack.push(Math.trunc(stack.pop() / Number(num)))
+//         }
+//         prevSign = input[i]
+//         num = ''
+//     }
+//     }
+//     console.log(stack);
+}
+
+parsethis("123*456+23+43")
--- grind75/medium/BasicCalc2/js/main.js
+++ grind75/medium/BasicCalc2/js/main.js
@@ -3,15 +3,15 @@
 //implement '*' and '/'. We truncate the decimal for division.
 //O(n) solution that uses a stack to keep track of the previous operator
 
-s.replace(/\s/g,'')
-
 let stack = []
 let prevsign = '+'
 let num = ''
 
 for( let i = 0; i<input.length; i++){
+    // if input[i] is a number. not NaN after number type change
     if(!isNaN(input[i])){
-        num==input[i]
+        // num is added to input[i]
+        num+=input[i]
     }
     if(isNaN(input[i]) || i == s.length - 1){
         if(preSign == '+'){
