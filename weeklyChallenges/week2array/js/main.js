c17c0293d34316d16e04d98225092b93a789925e
jordan Moldovan <jmoldyvan@gmail.com>
2022-10-13 20:23:38 -0700

    oct

--- weeklyChallenges/week2array/js/main.js
+++ weeklyChallenges/week2array/js/main.js
@@ -23,3 +23,35 @@ checkEquality(arr2, arr4) //false
 
 // day2
 
+// make 2 functions 
+// 2 things, 1 the array, the other is element in array
+// take the element and shift the element one place left or right depending on the function
+// always an array, element can be different ttypes, element is a single digit or a string, element always in the string
+// if on far left or right dont move, mutate original array
+
+function moveLeft(arr,element){
+    
+    let elementIndex = arr.indexOf(element)
+    console.log(elementIndex)
+    if(elementIndex!==0){
+        arr.splice(elementIndex,1)
+        arr.splice(elementIndex-1,0,element)
+    }
+    console.log(arr)
+}
+function moveRight(arr,element){
+
+    let elementIndex = arr.indexOf(element)
+    console.log(elementIndex)
+    if(elementIndex!==(arr.length-1)){
+        arr.splice(elementIndex,1)
+        arr.splice(elementIndex+1,0,element)
+    }
+    console.log(arr)
+
+}
+myArray = ['abc',  1, 2, 'Hey!',89,98,'xyz', 'carbs', 'special']
+console.log(moveLeft(myArray, 'xyz'))
+console.log(moveLeft(myArray, 'xyz'))
+console.log(moveRight(myArray, 'special'))
+console.log(moveRight(myArray, 2))
