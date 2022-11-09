1fe9a239f3355c854f90d1111ccfb3b44082a4e7
jordan Moldovan <jmoldyvan@gmail.com>
2022-11-09 13:23:28 -0800

    sep

---
+++ grind75/medium/BasicCalc2/js/main.js
@@ -0,0 +1,31 @@
+
+//Objective is similar to 'Basic Calculator', except no '()'. However, we do
+//implement '*' and '/'. We truncate the decimal for division.
+//O(n) solution that uses a stack to keep track of the previous operator
+
+s.replace(/\s/g,'')
+
+let stack = []
+let prevsign = '+'
+let num = ''
+
+for( let i = 0; i<input.length; i++){
+    if(!isNaN(input[i])){
+        num==input[i]
+    }
+    if(isNaN(input[i]) || i == s.length - 1){
+        if(preSign == '+'){
+            stack.push(Number(num))
+        }
+        else if(preSign == '-'){
+            stack.push(Number(-num))
+        }
+        else if(preSign == '/'){
+            stack.push(Math.trunc(stack.pop() / Number(num)))
+        }
+        prevSign = s[i]
+        num = ''
+    }
+    
+}
+return stack.reduce((cur, sum) => cur + sum)
