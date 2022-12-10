ea1751831509d55b416020f2843c164d4cd48200
jordan Moldovan <jmoldyvan@gmail.com>
2022-12-10 12:03:31 -0800

    dec

--- 7kyu/VowelsCount/js/main.js
+++ 7kyu/VowelsCount/js/main.js
@@ -1,4 +1,12 @@
-<<<<<<< HEAD
+// 2023
+function getCount(str) {
+  let vowels = str.split('').filter((x) => ['a', 'e', 'i', 'o', 'u'].includes(x))
+  return vowels.length;
+}
+
+
+
+
 function getCount(str) {
   let vowelsCount = 0;
   let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
@@ -12,19 +20,4 @@ function getCount(str) {
 // enter your majic here
 
 
-=======
-function getCount(str) {
-  let vowelsCount = 0;
-  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-  let yoyo = str.split('')
-  let intersect = yoyo.filter(Element => vowels.includes(Element));
-  intersect.forEach(element => {
-    vowelsCount += 1
-  });
-  return vowelsCount;
-}
-// enter your majic here
-
-
->>>>>>> ab797a2ef1ffb1728e4fd520a4d9b49cacd704ce
 console.log(getCount('abracadabra'))
