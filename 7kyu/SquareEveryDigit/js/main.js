25681ba774b3090170a113d874ef3259a7ae8263
jordan Moldovan <jmoldyvan@gmail.com>
2022-12-11 13:10:24 -0800

    dec

--- 7kyu/SquareEveryDigit/js/main.js
+++ 7kyu/SquareEveryDigit/js/main.js
@@ -1,3 +1,12 @@
+// 2023
+
+function squareDigits(num){
+    num = num.toString().split('')
+    return Number(num.map((x) => x*x).join(''));
+  }
+
+
+  
 function squareDigits(num) {
     num = num.toString().split('').map(x => x ** 2).join('')
     return +num
