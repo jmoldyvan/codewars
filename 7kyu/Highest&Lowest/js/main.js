c13674e2da82cf607dfe573b5b399d500d8ac85a
jordan Moldovan <jmoldyvan@gmail.com>
2022-12-12 13:44:25 -0800

    dec

--- 7kyu/Highest&Lowest/js/main.js
+++ 7kyu/Highest&Lowest/js/main.js
@@ -5,3 +5,23 @@ function highAndLow(numbers) {
 }
 
 highAndLow("1 9 3 4 -5")
+
+
+function highAndLow(numbers){
+    let high = {number:0}
+    let low = {number:0}
+    for(i=0;i<numbers.length-1;i++){
+      let element = Number(numbers[i])
+      if(high[number] > element){
+        continue
+      }
+      else{
+        high[number] = element}
+    
+    if(low[number] < element){
+      continue
+    }
+    else{low[number] = element}
+    }
+    console.log(high)
+  }
--- 7kyu/MiddleCharacter/js/main.js
+++ 7kyu/MiddleCharacter/js/main.js
@@ -1,3 +1,15 @@
+// 2023
+
+function getMiddle(s){
+  if(s.length%2==0){
+    return `${s[(s.length/2)-1]}${s[s.length/2]}`
+  }
+  if(!s.length%2==0){
+    return `${s[(s.length-1)/2]}`
+  }
+}
+
+
 function getMiddle(s) {
     if (s.length % 2 !== 0) {
         f = s.split('');
