4117d4556f8326e81cb69b0fc18f614ea2bb84fa
jordan moldovan <jmoldyvan@gmail.com>
2023-06-09 18:34:59 -0700

    practice

---
+++ weeklyChallenges/huntober/queue.js
@@ -0,0 +1,24 @@
+// In this problem, we are going to be implementing our own enqueue,
+//  dequeue, and size methods for the queue constructor we are creating,
+//   so we should be able to create new instances of the Queue.
+// The enqueue method takes in the item as a parameter, while the dequeue method does not.
+// The size method simply returns the number of items in the queue.
+
+class Queue {
+    // implement your Queue constructor here
+    construstor () {
+        this.list = []
+    }
+  };
+  
+  Queue.prototype.enqueue = function(item) {
+    this._queue.push(item);
+  };
+  
+  Queue.prototype.dequeue = function() {
+    return this._queue.shift();
+  };
+  
+  Queue.prototype.size = function() {
+    return this._queue.length;
+  };
