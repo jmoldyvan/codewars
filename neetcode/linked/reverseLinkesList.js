a91a62efc8e101ab30cbf33e2d0c0acff4bb69d7
jordan moldovan <jmoldyvan@gmail.com>
2023-06-12 20:21:21 -0700

    practice

---
+++ neetcode/linked/reverseLinkesList.js
@@ -0,0 +1,29 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null
+    let curr = head
+    let next = null
+    
+    while(curr!== null){
+        // save next
+        next = curr.next
+        // reverse
+        curr.next = prev
+        // advance prev and curr
+        prev = curr
+        curr = next
+    }
+    return prev;
+};
